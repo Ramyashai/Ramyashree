fdc53bea8da97997bbf771ac20e1e2355b97598c
Ramyashai <30362429+Ramyashai@users.noreply.github.com>
2017-07-22 09:42:20 +0530

    Update pos.c

--- pos.c
+++ pos.c
@@ -1,1 +1,18 @@
-ujgigihoijol
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("enter a num:");
+  scanf("%d",&num);
+  {
+  if(num<0)
+    printf("negative num");
+    {
+      else if(num>0)
+      printf("positive num");
+    } 
+    else
+    printf("you entered 0");
+  }
+  return 0;
+}
